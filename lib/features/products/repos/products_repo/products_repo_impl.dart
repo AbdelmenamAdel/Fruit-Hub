f815367c099f3331efffc0540bec52298a8e6023
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-12-24 00:46:12 +0200

    fix the conflict

--- lib/features/products/repos/products_repo/products_repo_impl.dart
+++ lib/features/products/repos/products_repo/products_repo_impl.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:dartz/dartz.dart';
 import 'package:fruit_hub/core/errors/failures.dart';
 import 'package:fruit_hub/core/services/backend/data_service.dart';
@@ -49,55 +48,54 @@ class ProductsRepoImpl extends ProductsRepo {
     }
   }
 }
-=======
-import 'package:dartz/dartz.dart';
-import 'package:fruit_hub/core/errors/failures.dart';
-import 'package:fruit_hub/core/services/backend/data_service.dart';
-import 'package:fruit_hub/core/utils/backend_endpoint.dart';
-import 'package:fruit_hub/features/products/entities/product_entity.dart';
-import 'package:fruit_hub/features/products/models/product_model.dart';
-import 'package:fruit_hub/features/products/repos/products_repo/products_repo.dart';
 
-class ProductsRepoImpl extends ProductsRepo {
-  final DatabaseService databaseService;
+// import 'package:dartz/dartz.dart';
+// import 'package:fruit_hub/core/errors/failures.dart';
+// import 'package:fruit_hub/core/services/backend/data_service.dart';
+// import 'package:fruit_hub/core/utils/backend_endpoint.dart';
+// import 'package:fruit_hub/features/products/entities/product_entity.dart';
+// import 'package:fruit_hub/features/products/models/product_model.dart';
+// import 'package:fruit_hub/features/products/repos/products_repo/products_repo.dart';
 
-  ProductsRepoImpl(this.databaseService);
-  @override
-  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() async {
-    try {
-      var data =
-          await databaseService.getData(
-                path: BackendEndpoint.getProducts,
-                query: {
-                  'limit': 10,
-                  'orderBy': 'sellingCount',
-                  'descending': true,
-                },
-              )
-              as List<Map<String, dynamic>>;
-      List<ProductEntity> products = data
-          .map((e) => ProductModel.fromJson(e).toEntity())
-          .toList();
-      return right(products);
-    } catch (e) {
-      return left(ServerFailure('Failed to get products'));
-    }
-  }
+// class ProductsRepoImpl extends ProductsRepo {
+//   final DatabaseService databaseService;
 
-  @override
-  Future<Either<Failure, List<ProductEntity>>> getProducts() async {
-    try {
-      var data =
-          await databaseService.getData(path: BackendEndpoint.getProducts)
-              as List<Map<String, dynamic>>;
+//   ProductsRepoImpl(this.databaseService);
+//   @override
+//   Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() async {
+//     try {
+//       var data =
+//           await databaseService.getData(
+//                 path: BackendEndpoint.getProducts,
+//                 query: {
+//                   'limit': 10,
+//                   'orderBy': 'sellingCount',
+//                   'descending': true,
+//                 },
+//               )
+//               as List<Map<String, dynamic>>;
+//       List<ProductEntity> products = data
+//           .map((e) => ProductModel.fromJson(e).toEntity())
+//           .toList();
+//       return right(products);
+//     } catch (e) {
+//       return left(ServerFailure('Failed to get products'));
+//     }
+//   }
 
-      List<ProductEntity> products = data
-          .map((e) => ProductModel.fromJson(e).toEntity())
-          .toList();
-      return right(products);
-    } catch (e) {
-      return left(ServerFailure('Failed to get products'));
-    }
-  }
-}
->>>>>>> 1e8efb7 (done payment with paypal)
+//   @override
+//   Future<Either<Failure, List<ProductEntity>>> getProducts() async {
+//     try {
+//       var data =
+//           await databaseService.getData(path: BackendEndpoint.getProducts)
+//               as List<Map<String, dynamic>>;
+
+//       List<ProductEntity> products = data
+//           .map((e) => ProductModel.fromJson(e).toEntity())
+//           .toList();
+//       return right(products);
+//     } catch (e) {
+//       return left(ServerFailure('Failed to get products'));
+//     }
+//   }
+// }
